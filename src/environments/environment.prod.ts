6c12f2fdf9d4c6f928cc5d47f7284b4054a6b761
VSKerberos <yunus.erdoagan@gmail..com>
2021-10-05 17:22:15 +0300

    prepare production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: false
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDgkrxyqPqureJJFwC4ViH3OQ42WExTiOk",
+    authDomain: "cost-computation.firebaseapp.com",
+    projectId: "cost-computation",
+    storageBucket: "cost-computation.appspot.com",
+    messagingSenderId: "843228566879",
+    appId: "1:843228566879:web:80026ee483478c3620d2ea",
+    measurementId: "G-XPB2CQHG4M"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig : {
     apiKey: "AIzaSyDgkrxyqPqureJJFwC4ViH3OQ42WExTiOk",
     authDomain: "cost-computation.firebaseapp.com",
