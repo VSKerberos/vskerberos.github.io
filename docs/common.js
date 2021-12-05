09c7b6feec45b5f8de0f1955e94067dfe5b4bb8c
VSKerberos <yunus.erdoagan@gmail..com>
2021-12-05 22:38:59 +0300

    remove ngrx-store

---
+++ docs/common.js
@@ -0,0 +1,28 @@
+"use strict";
+(self["webpackChunkcost_calculator"] = self["webpackChunkcost_calculator"] || []).push([[592],{
+
+/***/ 53176:
+/*!*********************************************************!*\
+  !*** ./src/app/pages/material/default-dialog-config.ts ***!
+  \*********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "defaultDialogConfig": () => (/* binding */ defaultDialogConfig)
+/* harmony export */ });
+/* harmony import */ var _angular_material_dialog__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/material/dialog */ 44958);
+
+function defaultDialogConfig() {
+  const dialogConfig = new _angular_material_dialog__WEBPACK_IMPORTED_MODULE_0__.MatDialogConfig();
+  dialogConfig.disableClose = true;
+  dialogConfig.autoFocus = true;
+  dialogConfig.width = '85%';
+  dialogConfig.height = '800px';
+  return dialogConfig;
+}
+
+/***/ })
+
+}]);
+//# sourceMappingURL=common.js.map
